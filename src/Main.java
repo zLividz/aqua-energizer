972112f8a55e24e01fb3ab5ab07c7b8443ba9dfb
Petrolevb-Asus <Vincebellec@gmail.com>
2012-06-07 11:07:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Petrolevb
+ *
+ */
+public class Main
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
