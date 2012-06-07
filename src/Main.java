3cd615d115b8210624f9a3e51483c41103f8cb39
Petrolevb-Asus <Vincebellec@gmail.com>
2012-06-07 11:26:48 +0200

    Ajouts divers

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,13 @@ public class Main
 {
 
     /**
+     * fonctions quelquonques
      * @param args
      */
     public static void main(String[] args)
     {
         // TODO Auto-generated method stub
-
+        System.out.println("coucou");
     }
 
 }
