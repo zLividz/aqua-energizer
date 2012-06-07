41d0ec084f1af0e0b74879f10f722fdd66fe232f
Petrolevb-Asus <Vincebellec@gmail.com>
2012-06-07 15:29:48 +0200

    Anulation d'exemple

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,8 @@
 public class Main
 {
 
-    /**
-     * Fonction principale
-     * @param args arguments de la ligne de commande
-     */
     public static void main(String[] args)
     {
-         System.out.println("coucou");
     }
 
 }
