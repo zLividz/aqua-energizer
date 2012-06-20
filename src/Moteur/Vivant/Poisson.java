b7636693a11d2eb7bc7d1bc2be2ed201cb64e0b1
Petrolevb-Asus <Vincebellec@gmail.com>
2012-06-20 13:48:18 +0200

    Ajout d'une direction  initiale au poisson, et historique court terme de son déplacement

--- src/Moteur/Vivant/Poisson.java
+++ src/Moteur/Vivant/Poisson.java
@@ -15,14 +15,23 @@ public class Poisson extends Moteur.Case implements Moteur.Vivant.IEtreVivant
     /**
      * Constructeur de la classe
      * @param p Position du poisson
+     * @param directionInitiale Direction initiale du poisson
      * @throws CaseException Excption levée si la position n'est pas correcte
      */
-    public Poisson(Position p) throws CaseException
-    { super(Constantes.Case.Poisson, p); }
+    public Poisson(Position p, Direction directionInitiale) throws CaseException
+    { super(Constantes.Case.Poisson, p); this.m_DeplacementPrecedent = directionInitiale; }
     
     @Override
     public void deplacement(Direction directionDeplacement)
     {
         
     }
+    
+    /**
+     * Récupère la direction du déplacement précédent du poisson
+     * @return Enumération de direction du poisson
+     * @see Direction
+     */
+    public Direction getDirectionPrecedente() { return this.m_DeplacementPrecedent; } 
+    private Direction m_DeplacementPrecedent;
 }
